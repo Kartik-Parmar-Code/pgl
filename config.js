584de1024cecc4c8bdd4b0db87b6e213e2303142
Kartik Parmar <83116676+hacker09809@users.noreply.github.com>
2024-06-19 13:55:05 +0530

    commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min1.jpg",
+    min2: "min1.jpg",
+    min3: "min1.jpg",
+    min4: "min1.jpg",
+    min5: "min1.jpg",
+    min6: "min1.jpg",
+
+    max1: "max1.png",
+    max2: "max2.png",
+    max3: "max3.png",
+    max4: "max4.png",
+    max5: "max5.png",
+    max6: "max6.png",
+}
